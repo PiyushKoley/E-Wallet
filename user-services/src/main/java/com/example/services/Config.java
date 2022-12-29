4a0c012649c31683aa6376058bf8ce4384dad72c
piyushkoley <piyushkoley@gmail.com>
2022-12-29 21:26:17 +0530

    implemented kafka and redis (written config) in user-services

---
+++ user-services/src/main/java/com/example/services/Config.java
@@ -0,0 +1,82 @@
+package com.example.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.requests.ProduceRequest;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Properties;
+
+@Configuration
+public class Config {
+
+    @Bean
+    LettuceConnectionFactory getConnectionFactory() {
+
+        RedisStandaloneConfiguration RSC = new RedisStandaloneConfiguration();
+
+        LettuceConnectionFactory LCF = new LettuceConnectionFactory(RSC);
+
+        return LCF;
+    }
+
+    @Bean
+    RedisTemplate<String,Object> getRedisTemplate() {
+
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+
+        RedisSerializer<String> stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+
+        JdkSerializationRedisSerializer JSRS = new JdkSerializationRedisSerializer();
+        redisTemplate.setValueSerializer(JSRS);
+        redisTemplate.setHashValueSerializer(JSRS);
+
+        redisTemplate.setConnectionFactory(getConnectionFactory());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
+
+
+    //****************** FOR KAFKA TEMPLATE ****************
+
+    @Bean
+    Properties kafkaProperties() {
+
+        Properties properties = new Properties();
+
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+
+        return properties;
+    }
+
+    ProducerFactory<String,String> getProducerFactory() {
+        return new DefaultKafkaProducerFactory(kafkaProperties());
+    }
+
+    @Bean
+    KafkaTemplate<String,String> kafkatemplate() {
+
+        return new KafkaTemplate<>(getProducerFactory());
+    }
+
+
+}
--- user-services/src/main/java/com/example/services/UserService.java
+++ user-services/src/main/java/com/example/services/UserService.java
@@ -1,7 +1,15 @@
 package com.example.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -9,20 +17,87 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    RedisTemplate<String,Object> redisTemplate;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    KafkaTemplate<String,String> kafkaTemplate;
+
+    private final String REDIS_PREFIX_KEY = "user::";
+
+    private final String CREATE_WALLET_TOPIC = "create_wallet"; //this is the name of the topic in kafka....
+
     void createUser(UserRequestDto userRequestDto) {
 
         UserEntity userEntity = UserConvertor.convertDtoToEntity(userRequestDto);
+
         userRepository.save(userEntity);
+        //first save it to mysql then save it to redis ... if it fails to save to sql then it will not get saved to redis..
+
+        //save it to redis cache...
+        saveToRedis(userEntity);
+
+        // when ever we create a user we will send request to wallet to create a wallet also, through kafka...
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("userName",userEntity.getUserName());
+
+        /*
+        ******** THIS WILL BE THE jsonObject *********
+            {
+                "userName" : "here we will have userName of the user"
+            }
+        * */
+        String message = jsonObject.toJSONString(); //this is string now but it carries whole object in it ....
+
+        //**************** here in kafkaTemplate 1-> topic, 2-> partition(key), 3-> message *****************
+        kafkaTemplate.send(CREATE_WALLET_TOPIC,userEntity.getUserName(),message);
+
+        // topic and partition we have to create manually through CMD only,, here we are just send message to topic ....
+    }
+
+    private void saveToRedis(UserEntity userEntity) {
+
+        Map map = objectMapper.convertValue(userEntity,Map.class);
+
+        String key = REDIS_PREFIX_KEY + userEntity.getUserName();
+        redisTemplate.opsForHash().putAll(key, map);
+
+        redisTemplate.expire(key, Duration.ofHours(12));
     }
 
     UserEntity getUserByUserName(String userName) throws Exception{
 
+        // trying to get the use from redis (database)....
+        String key = REDIS_PREFIX_KEY + userName;
+        Map map = redisTemplate.opsForHash().entries(key);
 
-        UserEntity userEntity = userRepository.findByUserName(userName);
+        if(map == null || map.size()==0) { // if redis does not contains the key and value then first get it from SQL
+                                            // and save it to redis also ...
 
-        if(userEntity == null) {
-            throw new Exception("user not found in dataBase");
+            UserEntity userEntity = userRepository.findByUserName(userName); // getting from mysql ..
+
+            if(userEntity != null) {
+                saveToRedis(userEntity);  // and saving to redis also...
+            }
+            else{
+                throw new Exception("user not found in dataBase");
+            }
+            return userEntity;
         }
+
+        //if found in the redis cache...
+        UserEntity userEntity = objectMapper.convertValue(map,UserEntity.class);
+
+
+
+//        UserEntity userEntity = userRepository.findByUserName(userName);
+//
+//        if(userEntity == null) {
+//            throw new Exception("user not found in dataBase");
+//        }
         return userEntity;
     }
 }
